473b857dd0a4b6b32a4ff7a347e107f8e9b7bff0
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-01 15:11:42 +0100

    feat(style): update style setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,69 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#7V5DFA",
+        secondary: "#9277FF",
+        "dark-blue-gray": "#1E2139",
+        "midnight-blue": "#252945",
+        "pale-lavender": "#DFE3FA",
+        "slate-blue": "#888EB0",
+        "light-slate-blue": "#7E88C3",
+        "dark-navy": "#0C0E16",
+        red: "#EC5757",
+        "pale-pink": "#9277FF",
+        "light-bg": "#F8F8F8",
+        charcoal: "#141625",
+      },
+      fontSize: {
+        "heading-l": [
+          "2.25rem",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.071rem",
+            fontWeight: "bold",
+          },
+        ],
+        "heading-m": [
+          "1.5rem",
+          {
+            lineHeight: "1.375rem",
+            letterSpacing: "-0.047rem",
+            fontWeight: "bold",
+          },
+        ],
+        "heading-s": [
+          "0.9375rem",
+          {
+            lineHeight: "1.5rem",
+            letterSpacing: "-0.015625rem",
+            fontWeight: "bold",
+          },
+        ],
+        "heading-s-variant": [
+          "0.9375rem",
+          {
+            lineHeight: "0.9375rem",
+            letterSpacing: "-0.015625rem",
+            fontWeight: "bold",
+          },
+        ],
+        body: [
+          "0.8125rem",
+          {
+            lineHeight: "1.125rem",
+            letterSpacing: "-0.00625rem",
+            fontWeight: "500",
+          },
+        ],
+        "body-variant": [
+          "0.8125rem",
+          {
+            lineHeight: "0.9375rem",
+            letterSpacing: "-0.015625rem",
+            fontWeight: "500",
+          },
+        ],
       },
     },
   },
