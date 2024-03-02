b64f7ad7496f8ab3ac283fdce1ce7f8422db9c5a
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-02 19:38:32 +0100

    feat(nav): update nav classnames

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -27,11 +27,11 @@ interface NavProps {
 
 const MobileNav: React.FC<NavProps> = ({ logo, themeSwitcher, avatar }) => {
   return (
-    <nav className="w-full flex justify-between bg-shadow-navy dark:bg-dark-blue-gray h-[72px] md:hidden">
+    <nav className="w-full flex justify-between bg-shadow-navy dark:bg-dark-blue-gray h-[72px] lg:hidden">
       {logo}
       <div className="flex h-full gap-6">
         {themeSwitcher}
-        <div className="grid place-items-center border-l-2 border-l-light-navy px-6">
+        <div className="grid place-items-center border-l border-l-light-navy px-6">
           {avatar}
         </div>
       </div>
@@ -45,11 +45,11 @@ const DesktopSidebar: React.FC<NavProps> = ({
   avatar,
 }) => {
   return (
-    <aside className="h-full justify-between bg-shadow-navy dark:bg-dark-blue-gray hidden md:flex flex-col">
+    <aside className="h-full justify-between bg-shadow-navy dark:bg-dark-blue-gray hidden lg:flex flex-col">
       {logo}
       <div className="flex flex-col w-full items-center gap-6">
         {themeSwitcher}
-        <div className="grid place-items-center border-l-2 border-light-navy px-6 md:py-6 v:px-0 md:border-t-2 md:border-l-0 md:w-full">
+        <div className="grid place-items-center border-l border-light-navy px-6 lg:py-6 v:px-0 lg:border-t lg:border-l-0 lg:w-full">
           {avatar}
         </div>
       </div>
