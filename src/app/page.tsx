76d64413a04eccff934892a0478615d7e925f062
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-03 17:24:13 +0100

    fix(log): remove console.log's

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,6 @@ import { Suspense } from "react";
 export default async function Home() {
   const invoices: Invoice[] = await fetchInvoices();
 
-  console.log("invcoies:", invoices);
-  console.log("invcoies:", invoices.length);
-
   return (
     <section className="bg-light-bg dark:bg-charcoal h-full w-full dark:text-white flex flex-col md:items-center px-6 py-8 sm:px-12 sm:py-16 lg:px-0 lg:py-20">
       <div className="flex flex-col w-full max-w-[730px] gap-y-8 sm:gap-y-13 lg:gap-y-16">
