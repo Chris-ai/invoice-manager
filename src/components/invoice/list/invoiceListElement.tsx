49688d97b55afe21d37a2125192459f2ed2ba871
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-06 21:59:03 +0100

    feat(invoices): update invoice list element view

--- src/components/invoice/list/invoiceListElement.tsx
+++ src/components/invoice/list/invoiceListElement.tsx
@@ -2,7 +2,7 @@ import { pages } from "@/app/pages";
 import { ArrowRight } from "@/components/icons/arrowRight";
 import { StatusLabel } from "@/components/status";
 import { Currency, Invoice, Status } from "@/data/types";
-import { formatPrice } from "@/utils";
+import { formatDate, formatPrice } from "@/utils";
 import Link from "next/link";
 
 export const InvoiceListElement: React.FC<{ invoice: Invoice }> = ({
@@ -13,14 +13,14 @@ export const InvoiceListElement: React.FC<{ invoice: Invoice }> = ({
       <MobileListElement
         id={invoice.id}
         name={invoice.client.name}
-        paymentDue={invoice.paymentDue.toLocaleDateString()}
+        paymentDue={invoice.paymentDue}
         amountDue={invoice.amountDue}
         status={invoice.status}
       />
       <DesktopListElement
         id={invoice.id}
         name={invoice.client.name}
-        paymentDue={invoice.paymentDue.toLocaleDateString()}
+        paymentDue={invoice.paymentDue}
         amountDue={invoice.amountDue}
         status={invoice.status}
       />
@@ -31,7 +31,7 @@ export const InvoiceListElement: React.FC<{ invoice: Invoice }> = ({
 interface InvoiceListElement {
   id: number;
   name: string;
-  paymentDue: string;
+  paymentDue: Date;
   amountDue: number;
   status: Status;
 }
@@ -45,14 +45,18 @@ export const MobileListElement: React.FC<InvoiceListElement> = ({
 }) => {
   return (
     <Link href={pages.invoice(id)}>
-      <div className="flex flex-col gap-6 p-6 dark:bg-dark-blue-gray bg-transparent shadow-lg rounded-lg md:hidden">
+      <div className="flex flex-col gap-6 p-6 dark:bg-dark-blue-gray bg-transparent shadow-sm rounded-lg md:hidden">
         <div className="flex justify-between items-center">
-          <p>{id}</p>
-          <p>{name}</p>
+          <p className="text-heading-s">#{id}</p>
+          <p className="text-light-slate-blue darK:pale-lavender text-body-variant">
+            {name}
+          </p>
         </div>
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-2">
-            <p>Due {paymentDue}</p>
+            <p className="text-light-slate-blue darK:pale-lavender text-body-variant">
+              Due {formatDate(paymentDue)}
+            </p>
             <p className="text-heading-s">
               {formatPrice(Currency.GBP, amountDue)}
             </p>
@@ -72,17 +76,23 @@ export const DesktopListElement: React.FC<InvoiceListElement> = ({
   status,
 }) => {
   return (
-    <div className="gap-6 p-4 dark:bg-dark-blue-gray bg-transparent shadow-lg rounded-lg hidden md:flex justify-between items-center">
-      <p>{id}</p>
-      <p>Due {paymentDue}</p>
-      <p>{name}</p>
-      <p className="text-heading-s text-end">
-        {formatPrice(Currency.GBP, amountDue)}
-      </p>
-      <StatusLabel status={status} />
-      <Link href={pages.invoice(id)}>
+    <Link href={pages.invoice(id)} className="group">
+      <div className="gap-6 p-4 dark:bg-dark-blue-gray bg-transparent shadow-sm rounded-lg hidden md:flex justify-between items-center border border-transparent group-active:border-primary">
+        <p className="text-heading-s">#{id}</p>
+        <p className="text-light-slate-blue darK:pale-lavender text-body-variant flex-1">
+          Due {formatDate(paymentDue)}
+        </p>
+        <p className="text-light-slate-blue dark:pale-lavender text-body-variant flex-1">
+          {name}
+        </p>
+        <p className="text-heading-s text-end flex-1 pr-4">
+          {formatPrice(Currency.GBP, amountDue)}
+        </p>
+        <div className="flex-1">
+          <StatusLabel status={status} />
+        </div>
         <ArrowRight />
-      </Link>
-    </div>
+      </div>{" "}
+    </Link>
   );
 };
