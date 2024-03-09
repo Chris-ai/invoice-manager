a3000d2fd777a384a84702686ffd64b50c7f66b4
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-09 23:19:38 +0100

    feat(invoicePage): udpate invoice page

--- src/app/invoice/[id]/page.tsx
+++ src/app/invoice/[id]/page.tsx
@@ -1,9 +1,17 @@
 import { GoBackButton } from "@/components/goBackButton";
 import { InvoiceDetails } from "@/components/invoice/view/invoiceDetails";
 import { InvoiceDetailsHeader } from "@/components/invoice/view/invoiceDetailsHeader";
-import { Invoice, Status } from "@/data/types";
+import { fetchInvoicebyId } from "@/data/queries";
+import { Status } from "@/data/types";
 
-export default function InvoicePage() {
+type PageProps = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export default async function InvoicePage({ params }: PageProps) {
+  const invoice = await fetchInvoicebyId(+params.id);
+
   return (
     <section className="bg-light-bg dark:bg-charcoal w-full dark:text-white flex flex-col md:items-center px-6 py-8 sm:px-12 sm:py-16 lg:px-0 lg:py-20">
       <div className="w-full max-w-[730px] mb-8">
@@ -11,7 +19,7 @@ export default function InvoicePage() {
       </div>
       <div className="flex flex-col w-full max-w-[730px] gap-y-8 sm:gap-y-13 lg:gap-y-16">
         <InvoiceDetailsHeader status={Status.PAID} />
-        <InvoiceDetails />
+        <InvoiceDetails invoice={invoice} />
       </div>
     </section>
   );
